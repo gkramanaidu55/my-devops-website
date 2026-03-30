894d2f140c045bbd643a3a5e17a242e1bb513848
gkramanaidu55 <gkramanaidu55@gmail.com>
2026-03-30 16:17:53 -0400

    updated hero message

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,10 @@ function Hero() {
   return (
     <section className="hero" id="home">
       <div className="container hero-content">
-        <h1>Build, Store, and Deploy with Confidence</h1>
+        <h1>Hi Prakash & Jaya 👋</h1>
         <p>
-          This sample website demonstrates a complete DevOps flow using React,
-          GitHub, GitHub Actions, JFrog Artifactory, and Azure.
+          How are you guys? 🚀 This website is deployed using CI/CD with GitHub,
+          JFrog, and Azure.
         </p>
         <a href="#features" className="hero-btn">
           Explore Features
